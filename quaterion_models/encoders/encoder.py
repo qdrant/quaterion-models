e1359f3af4e0d1ce61772d52a89fc993bb3e7984
George Panchuk <george.panchuk@qdrant.tech>
2022-03-15 12:37:19 +0300

    refactoring: replace reST with google-style, update docstrings #12

--- quaterion_models/encoders/encoder.py
+++ quaterion_models/encoders/encoder.py
@@ -8,68 +8,70 @@ from quaterion_models.types import TensorInterchange, CollateFnType
 
 
 class Encoder(nn.Module):
-    """
-    Base class for encoder abstraction
-    """
+    """Base class for encoder abstraction"""
 
     def __init__(self):
         super(Encoder, self).__init__()
 
     def disable_gradients_if_required(self):
-        """
-        Disables gradients of the model if it is declared as not trainable
-        :return:
-        """
+        """Disables gradients of the model if it is declared as not trainable"""
+
         if not self.trainable():
             for key, weights in self.named_parameters():
                 weights.requires_grad = False
 
     def trainable(self) -> bool:
-        """
-        Defines if encoder is trainable. This flag affects caching and checkpoint saving of the encoder.
-        :return: bool
+        """Defines if encoder is trainable. This flag affects caching and checkpoint saving of the
+        encoder.
+
+        Returns:
+            bool
         """
         raise NotImplementedError()
 
     def embedding_size(self) -> int:
         """
-        :return: Size of resulting embedding
+
+        Returns:
+            Size of resulting embedding
         """
         raise NotImplementedError()
 
     def get_collate_fn(self) -> CollateFnType:
-        """
-        Provides function that converts raw data batch into suitable model input
+        """Provides function that converts raw data batch into suitable model input
 
-        :return: Model input
+        Returns:
+             Model input
         """
         return default_collate
 
     def forward(self, batch: TensorInterchange) -> Tensor:
-        """
-        Infer encoder - convert input batch to embeddings
+        """Infer encoder - convert input batch to embeddings
 
-        :param batch: processed batch
-        :return: embeddings, shape: [batch_size x embedding_size]
+        Args:
+            batch: processed batch
+        Returns:
+            embeddings: shape: (batch_size, embedding_size)
         """
         raise NotImplementedError()
 
     def save(self, output_path: str):
-        """
-        Persist current state to the provided directory
+        """Persist current state to the provided directory
 
-        :param output_path:
-        :return:
+        Args:
+            output_path: path to save model
+
         """
         raise NotImplementedError()
 
     @classmethod
     def load(cls, input_path: str) -> Encoder:
-        """
-        Instantiate encoder from saved state.
+        """Instantiate encoder from saved state.
+
         If no state required - just call `create` instead
 
-        :param input_path:
-        :return:
+        Args:
+            input_path: path to load from
+
         """
         raise NotImplementedError()
