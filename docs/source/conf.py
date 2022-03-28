4f53766f87b5618e2475599d6f99c00d85dc18af
trean <trean.mi@gmail.com>
2022-03-28 16:32:38 +0200

    changed theme to Read the Docs and added some custom css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,7 +80,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -89,4 +89,10 @@ html_static_path = ["_static"]
 
 html_css_files = [
     "quaterion_models.css",
+    "css/custom.css"
 ]
+
+html_sidebars = {
+   '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'],
+   'using/windows': ['windowssidebar.html', 'searchbox.html'],
+}
