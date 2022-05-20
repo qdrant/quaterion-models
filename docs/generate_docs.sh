0d9baac38d509c0a2d30f79b01828a8e0b7f0a62
George Panchuk <george.panchuk@qdrant.tech>
2022-05-20 14:14:16 +0300

    fix: move package installation to the proper place, add extras #23

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -5,9 +5,5 @@ set -e
 # Ensure current path is project root
 cd "$(dirname "$0")/../"
 
-pip install poetry
-poetry build -f wheel
-pip install dist/$(ls -1 dist | grep .whl)
-
 poetry run sphinx-apidoc -f -e -o docs/source quaterion_models
 poetry run sphinx-build docs/source docs/html
