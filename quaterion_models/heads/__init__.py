2eb579c83c0e879824f8d524b79202280d569257
autoblack <qdrant@users.noreply.github.com>
2022-07-07 06:31:19 +0000

    fixup: Format Python code with Black

--- quaterion_models/heads/__init__.py
+++ quaterion_models/heads/__init__.py
@@ -4,6 +4,5 @@ from quaterion_models.heads.gated_head import GatedHead
 from quaterion_models.heads.sequential_head import SequentialHead
 from quaterion_models.heads.skip_connection_head import SkipConnectionHead
 from quaterion_models.heads.softmax_head import SoftmaxEmbeddingsHead
-from quaterion_models.heads.stacked_projection_head import \
-    StackedProjectionHead
+from quaterion_models.heads.stacked_projection_head import StackedProjectionHead
 from quaterion_models.heads.widening_head import WideningHead
