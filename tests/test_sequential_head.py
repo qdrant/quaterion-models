28ec4b4bf4a0b9521c30721909bcea309049a4da
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-07-12 17:30:32 +0300

    rm leftover code

--- tests/test_sequential_head.py
+++ tests/test_sequential_head.py
@@ -90,5 +90,3 @@ def test_save_and_load():
 
     assert type(model.head) == type(loaded_model.head)
     assert torch.allclose(origin_output, loaded_model.encode(batch, to_numpy=False))
-if __name__ == "__main__":
-    test_save_and_load()
