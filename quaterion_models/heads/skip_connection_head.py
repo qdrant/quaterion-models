c2203fb2e614ec6c78d7235178bb07a803550ace
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2022-12-08 12:02:21 +0300

    Stacked gated residuals in `SkipConnectionHead` (#46)
    * Add support for configurable number of stacked gated residuals
    
    * Bump version
    
    * User `regiser_parameter()` to set params dynamically

--- quaterion_models/heads/skip_connection_head.py
+++ quaterion_models/heads/skip_connection_head.py
@@ -34,21 +34,37 @@ class SkipConnectionHead(EncoderHead):
         skip_dropout:
             Additional dropout, applied to the trainable branch only.
             Using additional dropout allows to avoid the modification of original embedding.
+        n_layers:
+            Number of gated residual blocks stacked on top of each other.
     """
 
     def __init__(
-        self, input_embedding_size: int, dropout: float = 0.0, skip_dropout: float = 0.0
+        self,
+        input_embedding_size: int,
+        dropout: float = 0.0,
+        skip_dropout: float = 0.0,
+        n_layers: int = 1,
     ):
         super().__init__(input_embedding_size, dropout=dropout)
-        self.gates = Parameter(torch.Tensor(self.input_embedding_size))
-        self.reset_parameters()
-        self.fc = torch.nn.Linear(input_embedding_size, input_embedding_size)
-        self._skip_dropout_prob = skip_dropout
+        for i in range(n_layers):
+            self.register_parameter(
+                f"gates_{i}", Parameter(torch.Tensor(self.input_embedding_size))
+            )
+            setattr(
+                self,
+                f"fc_{i}",
+                torch.nn.Linear(input_embedding_size, input_embedding_size),
+            )
+
         self.skip_dropout = (
             torch.nn.Dropout(p=skip_dropout)
             if skip_dropout > 0.0
             else torch.nn.Identity()
         )
+
+        self._skip_dropout_prob = skip_dropout
+        self._n_layers = n_layers
+        self.reset_parameters()
 
     @property
     def output_size(self) -> int:
@@ -62,17 +78,25 @@ class SkipConnectionHead(EncoderHead):
         Returns:
             torch.Tensor: shape: (batch_size, input_embedding_size)
         """
-        return (
-            self.fc(self.skip_dropout(input_vectors)) * torch.sigmoid(self.gates)
-            + input_vectors
-        )
+        for i in range(self._n_layers):
+            fc = getattr(self, f"fc_{i}")
+            gates = getattr(self, f"gates_{i}")
+            input_vectors = (
+                fc(self.skip_dropout(input_vectors)) * torch.sigmoid(gates)
+                + input_vectors
+            )
+
+        return input_vectors
 
     def reset_parameters(self) -> None:
-        torch.nn.init.constant_(
-            self.gates, -4.0
-        )  # -4. ensures that all vector components are disabled by default
+        for i in range(self._n_layers):
+            torch.nn.init.constant_(
+                getattr(self, f"gates_{i}"), -4.0
+            )  # -4. ensures that all vector components are disabled by default
 
     def get_config_dict(self) -> Dict[str, Any]:
         config = super().get_config_dict()
-        config.update({"skip_dropout": self._skip_dropout_prob})
+        config.update(
+            {"skip_dropout": self._skip_dropout_prob, "n_layers": self._n_layers}
+        )
         return config
